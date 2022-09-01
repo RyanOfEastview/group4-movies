a9ecf016ee5572425965abaf66bfb69b1ba9d76f
RyanOfEastview <ryanmccarthy613@gmail.com>
2022-09-01 15:10:18 -0400

    issues with routes fixed

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,14 @@
 DROP TABLE IF EXISTS user;
 
-CREATE TABLE user (
+CREATE TABLE users (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(30) NOT NULL,
   email VARCHAR(30) NOT NULL,
   pw CHAR(128) NOT NULL
   );
 
--- CREATE TABLE movies (id INTEGER AUTO_INCREMENT PRIMARY KEY,
---   movie_title VARCHAR(30) NOT NULL,
---   movie_year VARCHAR(30) NOT NULL,
---   comments TEXT
--- );
+CREATE TABLE movies (id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  movie_title VARCHAR(30) NOT NULL,
+  movie_year VARCHAR(30) NOT NULL,
+  comments TEXT
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO user (username, email, pw)
+INSERT INTO users (username, email, pw)
 VALUES
   ('Ryan', 'Ryan@email.com', 'Ryan'),
   ('Jaime', 'Jaime@email.com', 'Jaime'),
@@ -10,3 +10,8 @@ VALUES
   ('Jill', 'Jill@email.com', 'Jill'),
   ('Venus', 'Venus@email.com', 'Venus'),
   ('Peanut', 'Peanut@email.com', 'Peanut');
+
+INSERT INTO movies (movie_title, movie_year, comments)
+VALUES
+('Forrest Gump', '1994', 'Wonderful movie with Oscar winning performances.'),
+('Office Space', '1999', 'Hilarious cult classic.');
