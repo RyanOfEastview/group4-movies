cd53d4038f8d4873f2f2e345e3d999906d1b880b
RyanOfEastview <ryanmccarthy613@gmail.com>
2022-09-02 11:46:10 -0400

    moving on to POST routes in a new branch

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,13 @@
+INSERT INTO movies (movie_title, movie_year, comments)
+VALUES
+('Forrest Gump', '1994', 'Wonderful movie with Oscar winning performances.'),
+('The Wedding Singer', '1998', 'One of Adam Sandlers better movies.  Not as whacky as Billy Madison or Happy Gilmore.  Great soundtrack.'),
+('Office Space', '1999', 'Hilarious cult classic.'),
+('Sin City', '2005', 'Very cool film noire.  I love the lack of colour except for in select scenes.'),
+('Pulp Fiction', '1994', 'John Travoltas career gets a revival with this one. Amazing cast.  Sam Jackson at his finest.'),
+('The Usual Suspects', '1995', 'A sole survivor tells of the twisty events leading up to a horrific gun battle on a boat, which began when five criminals met at a seemingly random police lineup.'),
+('Spider-Man: Into the Spider-Verse', '2018', 'Teen Miles Morales becomes the Spider-Man of his universe, and must join with five spider-powered individuals from other dimensions to stop a threat for all realities.');
+
 INSERT INTO users (username, email, pw)
 VALUES
   ('Ryan', 'Ryan@email.com', 'Ryan'),
@@ -10,14 +20,3 @@ VALUES
   ('Jill', 'Jill@email.com', 'Jill'),
   ('Venus', 'Venus@email.com', 'Venus'),
   ('Peanut', 'Peanut@email.com', 'Peanut');
-
-INSERT INTO movies (movie_title, movie_year, comments)
-VALUES
-('Forrest Gump', '1994', 'Wonderful movie with Oscar winning performances.'),
-('The Wedding Singer', '1998', 'One of Adam Sandlers better movies.  Not as whacky as Billy Madison or Happy Gilmore.  Great soundtrack.'),
-('Office Space', '1999', 'Hilarious cult classic.'),
-('Sin City', '2005', 'Very cool film noire.  I love the lack of colour except for in select scenes.'),
-('Pulp Fiction', '1994', 'John Travoltas career gets a revival with this one. Amazing cast.  Sam Jackson at his finest.'),
-('The Usual Suspects', '1995', 'A sole survivor tells of the twisty events leading up to a horrific gun battle on a boat, which began when five criminals met at a seemingly random police lineup.'),
-('Spider-Man: Into the Spider-Verse', '2018', 'Teen Miles Morales becomes the Spider-Man of his universe, and must join with five spider-powered individuals from other dimensions to stop a threat for all realities.'),
-;
