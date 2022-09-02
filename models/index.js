4801c5094bc387837fc6b3719333d89a92d6e539
RyanOfEastview <ryanmccarthy613@gmail.com>
2022-09-02 17:06:49 -0400

    Lost connection to db and don't know how

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
 //  all this file does right now is import the User model and export an object with it as a property
 
 const User = require("./User");
-const Post = require("./Post");
 
-module.exports = { User, Post };
+module.exports = { User };
--- models/post.js
+++ models/post.js
@@ -1,44 +1,44 @@
-const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../config/connection");
+// const { Model, DataTypes } = require("sequelize");
+// const sequelize = require("../config/connection");
 
-//create our Post model
-class Post extends Model {}
+// //create our Post model
+// class Post extends Model {}
 
-// create fields/columns for Post model  --  TO BE DISCUSSED for FIELDS
-Post.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    movie_title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    post_url: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isURL: true,
-      },
-    },
-    // THIS WILL BE THE USER ID OF THE PERSON LEAVIN THE COMMENT
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "user",
-        key: "id",
-      },
-    },
-  },
-  {
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: "post",
-  }
-);
+// // create fields/columns for Post model  --  TO BE DISCUSSED for FIELDS
+// Post.init(
+//   {
+//     id: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       primaryKey: true,
+//       autoIncrement: true,
+//     },
+//     movie_title: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     post_url: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//       validate: {
+//         isURL: true,
+//       },
+//     },
+//     // THIS WILL BE THE USER ID OF THE PERSON LEAVIN THE COMMENT
+//     user_id: {
+//       type: DataTypes.INTEGER,
+//       references: {
+//         model: "user",
+//         key: "id",
+//       },
+//     },
+//   },
+//   {
+//     sequelize,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: "post",
+//   }
+// );
 
-module.exports = Post;
+// module.exports = Post;
