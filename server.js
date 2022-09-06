52eba531d132a7c388975cdb431b5ae9283db32d
enceps <spence.d.cole@gmail.com>
2022-09-05 22:45:50 -0400

    add login page

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+const path = require('path');
 const express = require("express");
-const routes = require("./controllers");
+const routes = require("./controllers/");
 const sequelize = require("./config/connection");
 
 // Connects to the server
@@ -11,7 +12,7 @@ const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
