cc14951a05ce66cf51037e81194473e2d904c69b
RyanOfEastview <ryanmccarthy613@gmail.com>
2022-09-06 22:05:23 -0400

    back end working ish

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -15,13 +15,13 @@ const commentdata = [
   {
     comment_text: "Aliquam erat volutpat. In congue.",
     user_id: 3,
-    post_id: 10,
+    post_id: 1,
   },
   {
     comment_text:
       "Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.",
     user_id: 3,
-    post_id: 18,
+    post_id: 1,
   },
   {
     comment_text: "In hac habitasse platea dictumst.",
@@ -44,13 +44,13 @@ const commentdata = [
     comment_text:
       "Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat.",
     user_id: 6,
-    post_id: 12,
+    post_id: 2,
   },
   {
     comment_text:
       "Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.",
     user_id: 3,
-    post_id: 14,
+    post_id: 4,
   },
   {
     comment_text: "Donec ut mauris eget massa tempor convallis.",
@@ -67,7 +67,7 @@ const commentdata = [
     comment_text:
       "Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.",
     user_id: 5,
-    post_id: 14,
+    post_id: 4,
   },
   {
     comment_text: "Quisque porta volutpat erat.",
@@ -83,19 +83,19 @@ const commentdata = [
     comment_text:
       "Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis.",
     user_id: 4,
-    post_id: 11,
+    post_id: 1,
   },
   {
     comment_text:
       "Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est.",
     user_id: 5,
-    post_id: 13,
+    post_id: 6,
   },
   {
     comment_text:
       "Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.",
     user_id: 9,
-    post_id: 16,
+    post_id: 7,
   },
   {
     comment_text: "Curabitur convallis.",
@@ -106,7 +106,7 @@ const commentdata = [
     comment_text:
       "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
     user_id: 4,
-    post_id: 10,
+    post_id: 7,
   },
   {
     comment_text: "Morbi non quam nec dui luctus rutrum.",
@@ -117,13 +117,13 @@ const commentdata = [
     comment_text:
       "Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. Vestibulum ac est lacinia nisi venenatis tristique.",
     user_id: 8,
-    post_id: 10,
+    post_id: 8,
   },
   {
     comment_text:
       "Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.",
     user_id: 1,
-    post_id: 15,
+    post_id: 7,
   },
   {
     comment_text:
@@ -134,23 +134,23 @@ const commentdata = [
   {
     comment_text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
     user_id: 1,
-    post_id: 15,
+    post_id: 1,
   },
   {
     comment_text: "Nam tristique tortor eu pede.",
     user_id: 4,
-    post_id: 16,
+    post_id: 3,
   },
   {
     comment_text:
       "Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
     user_id: 4,
-    post_id: 18,
+    post_id: 1,
   },
   {
     comment_text: "Proin eu mi. Nulla ac enim.",
     user_id: 4,
-    post_id: 10,
+    post_id: 5,
   },
   {
     comment_text: "Sed ante. Vivamus tortor.",
@@ -159,7 +159,7 @@ const commentdata = [
   },
   {
     comment_text: "Aliquam quis turpis eget elit sodales scelerisque.",
-    user_id: 10,
+    user_id: 9,
     post_id: 1,
   },
   {
@@ -170,18 +170,18 @@ const commentdata = [
   {
     comment_text:
       "Curabitur gravida nisi at nibh. In hac habitasse platea dictumst.",
-    user_id: 10,
-    post_id: 14,
+    user_id: 1,
+    post_id: 4,
   },
   {
     comment_text: "Maecenas ut massa quis augue luctus tincidunt.",
-    user_id: 10,
+    user_id: 1,
     post_id: 8,
   },
   {
     comment_text: "Cras in purus eu magna vulputate luctus.",
-    user_id: 10,
-    post_id: 11,
+    user_id: 3,
+    post_id: 4,
   },
   {
     comment_text: "Etiam vel augue. Vestibulum rutrum rutrum neque.",
@@ -199,7 +199,7 @@ const commentdata = [
     comment_text:
       "Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc.",
     user_id: 2,
-    post_id: 11,
+    post_id: 5,
   },
   {
     comment_text: "Vestibulum ac est lacinia nisi venenatis tristique.",
@@ -220,19 +220,19 @@ const commentdata = [
   {
     comment_text:
       "Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.",
-    user_id: 10,
+    user_id: 1,
     post_id: 1,
   },
   {
     comment_text: "Proin risus. Praesent lectus.",
-    user_id: 10,
+    user_id: 1,
     post_id: 1,
   },
   {
     comment_text:
       "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus.",
-    user_id: 10,
-    post_id: 12,
+    user_id: 1,
+    post_id: 4,
   },
 ];
 
