2cbe47197096e6dbedf60fe3d1a82546d9671f84
Hiu Sum Jaime Yue <hiusumjaimeyue@cmail.carleton.ca>
2022-09-07 21:06:22 -0400

    Trying to do nodeemailer, need to move to back-end(routes)

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,8 @@ const Sequelize = require("sequelize");
 
 require("dotenv").config();
 
+// const nodemailer = require('nodemailer');
+
 //create connection to our database, pass in our MySQL information username and password
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -14,5 +16,34 @@ const sequelize = new Sequelize(
     port: 3306,
   }
 );
+
+//Send email using nodemailer
+
+// async function emailUser(name, toEmail) {
+//   let transport = nodemailer.createTransport({
+//     host: 'smtp.mailtrap.io',
+//     port: 2525,
+//     auth: {
+//       user: process.env.EMAIL_USERNAME,
+//       pass: process.env.EMAIL_PASSWORD
+//     }
+//   });
+
+//   var mailOptions = {
+//     from: 'jyonly108@gmail.com',
+//     to: toEmail,
+//     subject: 'Successfully sign up to Group 4 - Movies Reviewer',
+//     text: 'Welcome to Group 4 - Movies Reviewer, ' + name +
+//       '. We hope you enjoy our movies reviewer'
+//   };
+
+//   transport.sendMail(mailOptions, function (error, info) {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log('Email sent: ' + info.response);
+//     }
+//   });
+// }
 
 module.exports = sequelize;
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,3 +1,5 @@
+// const emailUser = require("./config/connection");
+
 async function signupFormHandler(event) {
   event.preventDefault();
 
@@ -23,6 +25,22 @@ async function signupFormHandler(event) {
     // check the response status
     if (response.ok) {
       console.log('success');
+      // var mailOptions = {
+      //   from: 'jyonly108@gmail.com',
+      //   to: email,
+      //   subject: 'Successfully sign up to Group 4 - Movies Reviewer',
+      //   text: 'Welcome to Group 4 - Movies Reviewer, ' + username +
+      //     '. We hope you enjoy our movies reviewer'
+      // };
+    
+      // transport.sendMail(mailOptions, function (error, info) {
+      //   if (error) {
+      //     console.log(error);
+      //   } else {
+      //     console.log('Email sent: ' + info.response);
+      //   }
+      // });
+      // emailUser(username, email);
       usernameEl.value = "";
       emailEl.value = "";
       pwEl.value = "";
@@ -56,6 +74,32 @@ async function loginFormHandler(event) {
     }
   }
 }
+
+// function emailUser(name, toEmail) {
+//   var transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//       user: 'jyonly108@gmail.com',
+//       pass: 'onlyForGame108'
+//     }
+//   });
+
+//   var mailOptions = {
+//     from: 'jyonly108@gmail.com',
+//     to: toEmail,
+//     subject: 'Successfully sign up to Group 4 - Movies Reviewer',
+//     text: 'Welcome to Group 4 - Movies Reviewer, ' + name +
+//       '. We hope you enjoy our movies reviewer'
+//   };
+
+//   transporter.sendMail(mailOptions, function (error, info) {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log('Email sent: ' + info.response);
+//     }
+//   });
+// }
 
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
